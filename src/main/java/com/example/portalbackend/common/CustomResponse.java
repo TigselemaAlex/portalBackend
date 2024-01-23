63889ba67dee3c918ae0eb7289959b76ecd53734
TigselemaAlex <kaiser.alex1994@gmail.com>
2024-01-23 15:03:54 -0500

    Custom response created

--- src/main/java/com/example/portalbackend/common/CustomResponse.java
+++ src/main/java/com/example/portalbackend/common/CustomResponse.java
@@ -1,4 +1,41 @@
 package com.example.portalbackend.common;
 
-public class CustomResponse {
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@JsonPropertyOrder({"status", "message", "data"})
+public class CustomResponse <T> {
+    private final HttpStatus status;
+    private final String message;
+    private final T data;
+
+    private CustomResponse(ResponseBuilder<T> builder){
+        this.status = builder.status;
+        this.message = builder.message;
+        this.data = builder.data;
+    }
+
+    public static class ResponseBuilder<T>{
+        private final HttpStatus status;
+        private String message;
+        private T data;
+
+        public ResponseBuilder(HttpStatus status, String message){
+            this.status = status;
+            this.message = message;
+        }
+
+        public ResponseBuilder<T> data (T data){
+            this.data = data;
+            return this;
+        }
+
+        public ResponseEntity<CustomResponse<?>> build(){
+            CustomResponse<T> customResponse = new CustomResponse<>(this);
+            return new ResponseEntity<>(customResponse, this.status);
+        }
+    }
 }
--- src/main/java/com/example/portalbackend/common/CustomResponseBuilder.java
+++ src/main/java/com/example/portalbackend/common/CustomResponseBuilder.java
@@ -1,4 +1,16 @@
 package com.example.portalbackend.common;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomResponseBuilder {
+
+    public ResponseEntity<CustomResponse<?>> build(HttpStatus status, String message){
+        return new CustomResponse.ResponseBuilder<>(status, message).build();
+    }
+    public <T> ResponseEntity<CustomResponse<?>> build(HttpStatus status, String message, T data){
+        return new CustomResponse.ResponseBuilder<>(status, message).data(data).build();
+    }
 }
